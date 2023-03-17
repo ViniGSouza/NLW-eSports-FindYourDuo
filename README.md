12db5f784b26b919f910a2bd495d19a5c890a0f9
ViniGSouza <vinizintwitchtv@gmail.com>
2023-03-17 17:15:16 -0300

    final version

---
+++ README.md
@@ -0,0 +1,30 @@
+# NLW - eSports
+
+<p align="center">
+  <img alt="" src=".github/capa.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+### Front-end
+- ReactJS
+- TailwindCSS
+- Radix UI
+
+### Back-end
+- NodeJS
+- Express
+- Prisma
+- SQLite
+
+### Mobile
+- React Native
+- Expo
+
+## Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para mais detalhes.
+
+---
+
+Feito por mim.
---
+++ web/src/App.tsx
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import logoImg from './assets/logo-nlw-esports.svg';
+import { CreateAdModal } from './components/CreateAdModal';
+import { GameBanner } from './components/GameBanner';
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
+
+function App() {
+  const [games, setGames] = useState<Game[]>([]);
+  useEffect(() => {
+    axios('http://localhost:3333/api/games').then(response => {
+      setGames(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        {games.map(game => {
+          return (
+            <GameBanner
+            key={game.id}
+            title={game.title}
+            bannerUrl={game.bannerUrl}
+            adsCount={game._count.ads}
+          />
+          )
+        })}
+      </div>
+
+        <CreateAdModal />
+    </div>
+  )
+}
+
+export default App
